4a2d202e8a9048d5acdef481a92704679eb79c41
Savannah Niles <savannahniles@gmail.com>
2013-07-23 15:26:42 -0500

    made edit page for section

--- app/helpers/sections_helper.rb
+++ app/helpers/sections_helper.rb
@@ -1,2 +1,8 @@
 module SectionsHelper
+
+	def capitalized_title(sentence)
+  		stop_words = %w{a an and the or for of nor} 
+  		sentence.split.each_with_index.map{|word, index| stop_words.include?(word) && index > 0 ? word : word.capitalize }.join(" ")
+	end
+
 end
